a4c902cffc7cb0e7c5599dacbcd8ad3aab12c015
Rebecca Sinkoff <rebecca.sinkoff@gmail.com>
2018-01-11 22:10:44 +0000

    remove_string_spaces from codewars.com

---
+++ remove_string_spaces.rb
@@ -0,0 +1,11 @@
+# from codewars.com
+
+# Simple, remove the spaces from the string, then return the resultant string.
+
+def no_space(x)
+  x = x.split(" ").join("")
+  puts x
+end
+
+
+no_space('jfBm  gk lf8hg  88lbe8 ')
